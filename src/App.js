a9e7a13c6a9d8b405dc900170012553955892032
Binamragupta <bgb221@nist.edu>
2023-08-17 23:04:15 +0530

    update

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename='/TazaKhabar'>
             <Navbar/>
             <LoadingBar
                 height={3}
@@ -32,14 +32,14 @@ export default class App extends Component {
                 
             />
             <Routes>
-            <Route exact path="/" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={"in"} category={"general"}/>}/>
-            <Route exact path="/business" element={<News setProgress={this.setProgress} key="Business" pageSize={this.pageSize} country={"in"} category={"business"}/>}/>
-            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key="" pageSize={this.pageSize} country={"in"} category={"entertainment"}/>}/>
-            <Route exact path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={"in"} category={"general"}/>}/>
-            <Route exact path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country={"in"} category={"health"}/>}/>
-            <Route exact path="/Science" element={<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country={"in"} category={"science"}/>}/>
-            <Route exact path="/Sports" element={<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country={"in"} category={"sports"}/>}/>
-            <Route exact path="/technology" element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country={"in"} category={"technology"}/>}/>
+            <Route exact path="/TazaKhabar" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={"in"} category={"general"}/>}/>
+            <Route path="/business" element={<News setProgress={this.setProgress} key="Business" pageSize={this.pageSize} country={"in"} category={"business"}/>}/>
+            <Route path="/entertainment" element={<News setProgress={this.setProgress} key="" pageSize={this.pageSize} country={"in"} category={"entertainment"}/>}/>
+            <Route path="/general" element={<News setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={"in"} category={"general"}/>}/>
+            <Route path="/health" element={<News setProgress={this.setProgress} key="health" pageSize={this.pageSize} country={"in"} category={"health"}/>}/>
+            <Route path="/Science" element={<News setProgress={this.setProgress} key="science" pageSize={this.pageSize} country={"in"} category={"science"}/>}/>
+            <Route path="/Sports" element={<News setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country={"in"} category={"sports"}/>}/>
+            <Route path="/technology" element={<News setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country={"in"} category={"technology"}/>}/>
             </Routes>
         </Router>
       </div>
